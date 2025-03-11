700af506a1f696b1d9d0a16f4b9f5db40af229b1
Akash Chekodu <chekodu.akash30@gmail.com>
2025-03-11 16:08:54 +0530

    Test webhook trigger

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout << "My SRN is PES1UG22AM016"
   cout << "I have successfully built and run this  \n";
   return 0;
 }
