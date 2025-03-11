8766d16b98c3b489645132197b4a9daa1b46b3a1
Akash Chekodu <chekodu.akash30@gmail.com>
2025-03-11 16:14:31 +0530

    fix update

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "My SRN is PES1UG22AM016"
-  cout << "This is CC LAB 7"
+  cout << "My SRN is PES1UG22AM016";
+  cout << "This is CC LAB 7";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
