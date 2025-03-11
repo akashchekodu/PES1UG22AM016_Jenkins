977b50cc4617147884d1fc5e734e0fcb1713a60b
Akash Chekodu <chekodu.akash30@gmail.com>
2025-03-11 16:12:57 +0530

    update

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "My SRN is PES1UG22AM016"
+  cout << "This is CC LAB 7"
   cout << "I have successfully built and run this  \n";
   return 0;
 }
